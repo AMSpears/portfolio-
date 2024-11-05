23ec0fc674a6a794fe5304cdf57db3a3a169a291
Angie Spears <angiespears@Angies-MacBook-Air.local>
2024-11-05 13:36:38 -0800

    adjust bio style

--- src/components/specific/HomeAbout.js
+++ src/components/specific/HomeAbout.js
@@ -4,7 +4,7 @@ import React from 'react';
 const HomeAbout = () => { 
 
   return (
-    <div className="relative w-full md:py-[196px] md:px-[96px] md:text-[48px] flex items-center justify-center text-white">
+    <div className="relative w-full py-[64px] px-[20px] md:py-[196px] md:px-[96px] md:text-[48px] flex items-center justify-center text-white text-center font-light text[24px] md:text-5xl">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
     </div>
   )

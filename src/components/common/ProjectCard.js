cf1ff07de72f9bb85c8586ce819ead1e1342771e
Angie Spears <angiespears@Angies-Air.attlocal.net>
2024-11-11 17:16:11 -0600

    create project data; adjust project cards data

--- src/components/common/ProjectCard.js
+++ src/components/common/ProjectCard.js
@@ -1,23 +1,24 @@
 import React from "react";
 
-const ProjectCard = ({ title, description, videoUrl, outerBackgroundColor, innerBackgroundColor, technologies }) => {
+const ProjectCard = ({ backgroundImage, backgroundColor, description, title, titleColor, textColor, technologies, videoUrl}) => {
   return (
-    <div className={`md:h-lvh ${outerBackgroundColor} shadow-md rounded-md py-[24px] px-[8px] md:p-[32px] mx-[20px] flex flex-col mb-[32px] md:mb-0 md:flex-row md:gap-32`}>
-      <h3 className="text-[32px] mb-[24px] font-semibold text-secondary uppercase md:hidden">{title}</h3>
+    <div className={`md:h-lvh ${backgroundColor} shadow-md rounded-md py-[24px] px-[8px] md:p-[32px] mx-[20px] flex flex-col mb-[32px] md:mb-0 md:flex-row md:gap-32`}>
+      <h3 className={`text-[32px] mb-[24px] ${titleColor} font-semibold uppercase md:hidden`}>{title}</h3>
       <div className="flex flex-col md:w-1/4 order-last md:order-first">
-        <h3 className="md:text-5xl mb-[24px] font-semibold text-secondary uppercase hidden md:block">{title}</h3>
-        <p className="text-lg text-secondary text-lg font-light">{description}</p>
+        <h3 className={`md:text-5xl mb-[24px] ${titleColor} font-semibold uppercase hidden md:block`}>{title}</h3>
+        <p className={` text-lg text-lg ${textColor} font-light`}>{description}</p>
         <div className="flex flex-wrap mt-[20px]">
           {technologies.map((technology, index) => (
             <p key={index} className="mr-[10px] mb-[10px] text-sm text-black bg-white py-[8px] px-[24px] rounded-2xl">{technology}</p>
           ))}
         </div>
       </div>
-      <div className={`${innerBackgroundColor} py-[24px] px-[8px] mb-[24px] md:mb-0 md:py-[100px] md:px-[50px] md:w-3/4 rounded-lg`}>
+      <div className={`${backgroundImage} bg-cover bg-center py-[24px] px-[8px] mb-[24px] md:mb-0 md:py-[100px] md:px-[50px] md:w-3/4 rounded-lg`}>
         <div className="w-full aspect-video">
-          <video className={`w-full h-full object-cover ${innerBackgroundColor} rounded-md`} controls>
+          <video className="w-full h-full object-cover rounded-md" controls>
             <source src={videoUrl} type="video/mp4" />
             Your browser does not support the video tag.
+            <track kind="captions" />
           </video>
         </div>
       </div>
--- src/components/specific/HomeProjects.js
+++ src/components/specific/HomeProjects.js
@@ -1,30 +1,11 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import ProjectCard from '../common/ProjectCard';
 import { Swiper, SwiperSlide } from "swiper/react";
+import projectData from '../data/projectData.json'; 
 
 import "swiper/css";
 
 const HomeProjects = () => {  
-
-const projects = [
-  {
-    title: "CUUP",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna nisi, consectetur sagittis lobortis ut, tempus quis orci. Vivamus placerat ligula eget est semper tincidunt. Pellentesque eget ullamcorper leo, quis dictum justo. Integer tempus lobortis nibh id euismod.",
-    videoUrl: "https://res.cloudinary.com/dh9cghj6y/video/upload/v1633110808/Portfolio%20Website/Projects/CUUP.mp4",
-    outerBackgroundColor: "bg-blue-400",
-    innerBackgroundColor: "bg-gray-300",
-    technologies: ["React", "Shopify", "GraphQL", "SCSS"]
-  },
-  {
-    title: "CUUP 2",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent magna nisi, consectetur sagittis lobortis ut, tempus quis orci. Vivamus placerat ligula eget est semper tincidunt. Pellentesque eget ullamcorper leo, quis dictum justo. Integer tempus lobortis nibh id euismod.",
-    videoUrl: "https://res.cloudinary.com/dh9cghj6y/video/upload/v1633110808/Portfolio%20Website/Projects/CUUP.mp4",
-    outerBackgroundColor: "bg-blue-400",
-    innerBackgroundColor: "bg-gray-300",
-    technologies: ["React", "Shopify", "GraphQL", "SCSS"]
-  },
-]
-
   return (
     <div className="relative w-full py-[48px] flex flex-col">
      <h2 className="ml-[20px] mb-[32px] md:mb-[48px] md:ml-[64px] text-primary text-[28px] md:text-5xl">Selected Projects</h2>
@@ -33,31 +14,22 @@ const projects = [
         slidesPerView={1.2}
         spaceBetween={32}
         className="mySwiper">        
-          {projects.map((project, index) => (
+          {projectData.map((project, index) => (
             <SwiperSlide key={index}>
               <ProjectCard
-                title={project.title}
-                description={project.description}
-                videoUrl={project.videoUrl}
-                outerBackgroundColor={project.outerBackgroundColor}
-                innerBackgroundColor={project.innerBackgroundColor}
-                technologies={project.technologies}
+                {...project}
               />
             </SwiperSlide>
           ))}
       </Swiper>
      </div>
      <div className='block md:hidden'>
-      {projects.map((project, index) => (
-        <ProjectCard
-          index={index}
-          title={project.title}
-          description={project.description}
-          videoUrl={project.videoUrl}
-          outerBackgroundColor={project.outerBackgroundColor}
-          innerBackgroundColor={project.innerBackgroundColor}
-          technologies={project.technologies}
-        />
+      {projectData.map((project, index) => (
+        <div key={index}> 
+          <ProjectCard
+            {...project}
+          />
+        </div>
       ))}
      </div>
     </div>

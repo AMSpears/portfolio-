3972bab30695430f3a6d19dc5e2ebeff47874c21
Angie Spears <angiespears@Angies-Air.attlocal.net>
2024-11-12 12:39:08 -0600

    update nav func

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,15 +1,34 @@
-import React, { useState } from 'react';
+import React, {  useEffect, useState } from 'react';
 import { Link } from 'gatsby';
 
 const Header = ({ logo }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [isFixed, setIsFixed] = useState(false);
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
 
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="flex justify-between items-center p-[20px] md:px-[80px] md:py-[24px] text-white">
+    <header className={`flex justify-between items-center p-[20px] md:px-[80px] md:py-[24px] text-white transition-all ${
+      isFixed ? 'fixed top-0 left-0 right-0 z-50' : 'relative' 
+    }`}>      
       <div className="flex items-center">
         { logo ? <img src={logo} alt="Logo" className="h-8" /> : <Link to="/">Angie Spears</Link> }
       </div>

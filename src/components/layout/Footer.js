ab924c461eddb5d343a7c100c9e928595be13147
Angie Spears <angiespears@Angies-Air.lan>
2024-11-20 10:54:58 -0800

    adjust footer alignment

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,23 +5,23 @@ const Footer = () => {
 
   return (
     <footer className="footer bg-footer-gradient text-white py-[48px] px-[20px] md:px-[96px]">
-      <div className="flex mb-[48px] md:mb-[100px] flex-col md:flex-row justify-between">
-        <div className="md:w-1/2">
+      <div className="mb-[48px] md:mb-[100px] flex flex-col md:flex-row justify-between">
+        <div className="md:w-1/2 text-center md:text-left">
           <h2 className="text-primary font-bold text-[44px] md:text-[100px] mb-[24px]">Get in Touch</h2>
           <a className="block mb-[48px] text-[24px] font-light" href="mailto:contact@angiespears.com">contact@angiespears.com</a>
         </div>
-          <p className="md:w-1/4 mb-[48px] text-[24px] md:text-[24px] font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+          <p className="md:w-1/4 mb-[48px] text-[24px] md:text-[24px] font-light">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
       </div>
       <div> 
-        <h3 className="text-primary text-[32px] mb-[24px]">Angie Spears</h3>
+        <h3 className="text-primary text-[32px] mb-[24px] text-center md:text-left">Angie Spears</h3>
         <div className="flex flex-col md:flex-row justify-between">
-          <div className="flex space-x-4 mb-[24px] text-[20px] font-light">
+          <div className="flex space-x-4 justify-center  md:justify-between mb-[24px] text-[20px] font-light">
             <a href="/about">About</a>
             <a href="/" target="_blank">Github</a>
             <a href="/" target='_blank'>Resume</a>
             <a href="mailto:contact@angiespears.com">Contact</a>
           </div>
-          < div className="flex space-x-4">
+          < div className="flex space-x-4 justify-center md:justify-between">
             <a href="/"> 
               <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none">
                 <circle cx="24" cy="24" r="24" fill="#FCFC8B"/>
